14326a74e8cfe722f3167b8df773067c4ee6baee
Alexander Ott <ajott@mtu.edu>
2020-12-16 13:27:54 -0500

    Quote command v1
    First crack at allowing the Deputy Admiral Bot to pull from the Google Sheets "database" of quotes.

---
+++ src/commands/quote.js
@@ -0,0 +1,16 @@
+const getURL = 'https://script.google.com/macros/s/AKfycbwbXKUIzHxXVY-pjKlx4SaY9EE85JmnkhgPlc4Ho_5XU8Nk56yI/exec'
+
+module.exports = {
+    name: 'quote',
+    description: "Get a quote from the \"database\".",
+    execute(message, args) {
+
+        const response = await fetch(getURL);
+
+        const quoteArr = await response.text().split("`");
+
+        const quote = quoteArr[Math.random() * (quoteArr.length - 2)]
+
+        message.channel.send("Here's a quote:\n\n>||"+quote+"||\n\n*These quotes are anonymized, and are in no way meant to be defamatory.*");
+    }
+}
