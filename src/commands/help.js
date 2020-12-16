90a914114e2ad4029d0b65331dc7fcc32b3d16e5
asrospie <asrospierski@gmail.com>
2020-12-16 15:10:20 -0500

    Fixed bug in quote added quote to help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(message, args) {
 
         // This needs to be re-formatted, working on it!!
-        const msg = `Here's what I can do!\n!new : If you're new, use this command for the onboarding process (must be used in onboarding)\n!iam : Used in the onboarding process. Use this with one of the following roles: friend, active, alumni (must be used in onboarding)\n!help : use this command if you need help\n!ping : Try it!\nThe Deputy Admiral Bot was created by Br. Adm. Alec Rospierski.\nAny bugs that are found are to be reported to the Office of the Admiral.`;
+        const msg = `Here's what I can do!\n\n!new : If you're new, use this command for the onboarding process (must be used in onboarding)\n!iam : Used in the onboarding process. Use this with one of the following roles: friend, active, alumni (must be used in onboarding)\n!help : use this command if you need help\n!ping : Try it!\n!quote : Retrieves a random quote from Alumni... viewer discretion advised.\n\nThe Deputy Admiral Bot was created by Br. Adm. Alec Rospierski.\nAny bugs that are found are to be reported to the Office of the Admiral.`;
         message.channel.send(msg);
     }
 }
--- src/commands/quote.js
+++ src/commands/quote.js
@@ -7,7 +7,7 @@ module.exports = {
     async execute(message, args) {
         const snark = ["I can't believe someone actually said this", "Who on earth would say a thing like this", "These are anonymous, not judgement-free", "And yet we __still__ let this person join", ":person_facepalming: This is a doozy", "Here, just take it", "You made me read this with my own ~~eyes~~ code"];
 
-        const snarksnip = snark[Math.random() * snark.length - 1];
+        const snarksnip = snark[Math.floor(Math.random() * snark.length)];
 
         // fetch response from google app script
         const response = await fetch(getURL);
