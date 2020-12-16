e6a86526608dccd3a09cbabf6b8b499ef4cfb7b0
Alexander Ott <ajott@mtu.edu>
2020-12-16 14:57:16 -0500

    Removed embed for the link, added snark

--- src/commands/quote.js
+++ src/commands/quote.js
@@ -4,6 +4,9 @@ module.exports = {
     name: 'quote',
     description: "Get a quote from the \"database\".",
     execute(message, args) {
+        const snark = ["I can't believe someone actually said this", "Who on earth would say a thing like this", "These are anonymous, not judgement-free", "And yet we __still__ let this person join", ":person_facepalming: This is a doozy", "Here, just take it", "You made me read this with my own ~~eyes~~ code"]
+
+        const snarksnip = snark[Math.random() * snark.length - 1]
 
         const response = await fetch(getURL);
 
@@ -11,6 +14,6 @@ module.exports = {
 
         const quote = quoteArr[Math.random() * (quoteArr.length - 2)]
 
-        message.channel.send("Here's a quote:\n\n> ||"+quote+"||\n*These quotes are anonymized, and are in no way meant to be defamatory.*\nIf you'd like to add a quote, you can enter it here: https://bit.ly/3gTJLl3");
+        message.channel.send(snarksnip+":\n> "+quote+"\n*These quotes are anonymized, and are in no way meant to be defamatory.*\nIf you'd like to add a quote, you can enter it here: <https://bit.ly/3gTJLl3>");
     }
 }
