28e3ab6f61c6c0d7d67a147342bbaadb522bb21a
Alexander Ott <ajott@mtu.edu>
2020-12-16 16:10:28 -0500

    Added more snark

--- src/commands/quote.js
+++ src/commands/quote.js
@@ -5,9 +5,12 @@ module.exports = {
     name: 'quote',
     description: "Get a quote from the \"database\".",
     async execute(message, args) {
-        const snark = ["I can't believe someone actually said this", "Who on earth would say a thing like this", "These are anonymous, not judgement-free", "And yet we __still__ let this person join", ":person_facepalming: This is a doozy", "Here, just take it", "You made me read this with my own ~~eyes~~ code"];
 
-        const snarksnip = snark[Math.random() * snark.length - 1];
+        // Why not give the bot some personality?
+        const snark = ["I can't believe someone actually said this.", "Who on earth would say a thing like this?", "These are anonymous, not judgement-free.", "And yet we __still__ let this person join!", ":person_facepalming: This is a doozy.", "Here, just take it:", "You made me read this with my own ~~eyes~~ code?", "Comin' in hot!", "Here's a winner:", "Served up fresh.", "Look, it was a different time.", "Yeah, sure, why not.", "Context? Who needs context?", "Careful not to cut yourself on this edge! :knife:", "This one'll make someone salty.", "Beep boop :robot:, `ERROR in function fetch(getURL): line 69 failed to execu` - nah, I'm just messing with you."];
+
+        // Grabs a random bit of snark
+        const snarkSnip = snark[Math.random() * snark.length];
 
         // fetch response from google app script
         const response = await fetch(getURL);
@@ -23,11 +26,11 @@ module.exports = {
             quote = quote.substring(1, quote.length);
         }
 
-        // check if last charact is a comma
+        // check if last character is a comma
         if (quote.charAt(quote.length - 1) === ',') {
             quote = quote.substring(0, quote.length - 1);
         }
 
-        message.channel.send(snarksnip+":\n> "+quote+"\n*These quotes are anonymized, and are in no way meant to be defamatory.*\nIf you'd like to add a quote, you can enter it here: <https://bit.ly/3gTJLl3>");
+        message.channel.send(snarkSnip+"\n> "+quote+"\n\n*These quotes are anonymized, and are in no way meant to be defamatory.*\nIf you'd like to add a quote, you can enter it here: <https://bit.ly/3gTJLl3>");
     }
 }
