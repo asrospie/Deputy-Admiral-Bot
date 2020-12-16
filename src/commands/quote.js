9b84768fd4086b8cdfae63b7dfb6849f5068f0fb
asrospie <asrospierski@gmail.com>
2020-12-16 14:01:08 -0500

    Installed fetch package, reformatted awaits, check for commas at beginning and end

--- src/commands/quote.js
+++ src/commands/quote.js
@@ -1,15 +1,29 @@
+const fetch = require('node-fetch');
 const getURL = 'https://script.google.com/macros/s/AKfycbwbXKUIzHxXVY-pjKlx4SaY9EE85JmnkhgPlc4Ho_5XU8Nk56yI/exec'
 
 module.exports = {
     name: 'quote',
     description: "Get a quote from the \"database\".",
-    execute(message, args) {
+    async execute(message, args) {
 
+        // fetch response from google app script
         const response = await fetch(getURL);
 
-        const quoteArr = await response.text().split("`");
+        // await response -> text, then split text into an array of quotes
+        const respText = await response.text();
+        const quoteArr = respText.split('`');
 
-        const quote = quoteArr[Math.random() * (quoteArr.length - 2)]
+        let quote = quoteArr[Math.floor(Math.random() * quoteArr.length)];
+
+        // check if first character is a comma
+        if (quote.charAt(0) === ',') {
+            quote = quote.substring(1, quote.length);
+        }
+
+        // check if last charact is a comma
+        if (quote.charAt(quote.length - 1) === ',') {
+            quote = quote.substring(0, quote.length - 1);
+        }
 
         message.channel.send("Here's a quote:\n\n>||"+quote+"||\n\n*These quotes are anonymized, and are in no way meant to be defamatory.*");
     }
