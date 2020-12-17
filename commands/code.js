0d40169854a56b895a8f0f9b2a0934797a452708
asrospie <asrospierski@gmail.com>
2020-12-17 18:41:46 -0500

    Remove unnecessary folder

--- commands/code.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'code',
-    description: "Use this to see how I'm made",
-    execute(message, args) {
-
-        // post the link
-        message.channel.send('https://github.com/asrospie/Deputy-Admiral-Bot');
-    }
-}
--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: "this is a test command",
-    execute(message, args) {
-        message.reply('pong!');
-    }
-}
